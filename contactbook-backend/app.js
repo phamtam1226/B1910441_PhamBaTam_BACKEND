592277ac6e7a892dd22618d09c827ebb5cbf8408
tamb1910441 <117994899+tamb1910441@users.noreply.github.com>
2023-02-23 21:19:47 +0700

    Dinh nghia cac route cho tai nguyen contact

--- contactbook-backend/app.js
+++ contactbook-backend/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
- 
- const app = express();
 
- app.use(cors());
- app.use(express.json());
+const contactsRouter = require("./app/routes/contact.route")
 
- app.get("/", (req, res) => {
-    res.json({message: "Welcome to contact book application."});
- });
+const app = express();
 
- module.exports = app;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+   res.json({ message: "Welcome to contact book application." });
+});
+app.use("/api/contacts", contactsRouter);
+
+module.exports = app;
